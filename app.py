46ed0a6177e03689bf7f608c6ce631962b409941
baemsu <83949128+baemsu@users.noreply.github.com>
2022-02-08 15:26:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import r2_score
+#from sklearn.ensemble import RandomForestClassifier
+#from sklearn.linear_model import LinearRegression
+#from sklearn.metrics import mean_squared_error
+#from sklearn.metrics import r2_score
 import joblib
 
 
